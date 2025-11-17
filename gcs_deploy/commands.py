3284e1d87d2bb588bc6f9cfafa5366ea0ea2bc21
Felipe Jimenez-Ibarra <felipejimenezibarra@gmail.com>
2025-11-18 09:41:24 +1100

    Set a more simple deploy. Just endpoint is deployed using globus-connect-server.

--- gcs_deploy/commands.py
+++ gcs_deploy/commands.py
@@ -21,71 +21,10 @@ def run_command(cmd, capture_output=False):
         return result.stdout.strip()
 
 
-
-
-
 def read_json(path):
     with open(path) as f:
         return json.load(f)
     
-    
-def get_id_by_name(
-        name,
-        type,
-        GCS_CLI_CLIENT_ID=None,
-        GCS_CLI_CLIENT_SECRET=None,
-        GCS_CLI_ENDPOINT_ID=None,
-    ):
-    
-    env_vars = ""
-    if GCS_CLI_CLIENT_ID and GCS_CLI_CLIENT_SECRET and GCS_CLI_ENDPOINT_ID:
-        env_vars = (
-            f"GCS_CLI_CLIENT_ID={GCS_CLI_CLIENT_ID} "
-            f"GCS_CLI_CLIENT_SECRET={GCS_CLI_CLIENT_SECRET} "
-            f"GCS_CLI_ENDPOINT_ID={GCS_CLI_ENDPOINT_ID} "
-        )
-
-    if type == "gateway":    
-        cmd = (
-            f"{env_vars}"
-            f"globus-connect-server storage-gateway list --format json"
-        )
-        output_with_envelop = run_command(
-            cmd,
-            capture_output=True
-        )
-        
-        output_with_envelop_json = json.loads(output_with_envelop)
-        gateway_list = output_with_envelop_json[0]["data"]
-
-        for gateway in gateway_list:
-            gateway_name = gateway["display_name"]
-            gateway_id = gateway["id"]
-            if gateway_name == name:
-                return gateway_id
-        
-
-    elif type == "collection":
-        cmd = (
-            f"{env_vars}"
-            f"globus-connect-server collection list --format json"
-        )
-
-        output = run_command(
-            cmd,
-            capture_output=True
-        )
-
-        collection_list = json.loads(output)
-
-        for collection in collection_list:
-            collection_name = collection["display_name"]
-            collection_id = collection["id"]
-            if collection_name == name:
-                return collection_id
-        
-    else:
-        raise ValueError(f"Unknown type: {type}")
 
 def setup_endpoint(config):
 
@@ -94,8 +33,6 @@ def setup_endpoint(config):
     organization = endpoint_config["organization"]
     contact_email = endpoint_config["contact_email"]
     project_name = endpoint_config["project_name"]
-    project_id = endpoint_config["project_id"]
-    private = endpoint_config["private"]
     GCS_CLI_CLIENT_ID = config.get("GCS_CLI_CLIENT_ID")
     GCS_CLI_CLIENT_SECRET = config.get("GCS_CLI_CLIENT_SECRET")
 
@@ -104,7 +41,7 @@ def setup_endpoint(config):
         f"GCS_CLI_CLIENT_SECRET={GCS_CLI_CLIENT_SECRET} "
         f"globus-connect-server endpoint setup \"{display_name}\" "
         f"--owner \"{GCS_CLI_CLIENT_ID}@clients.auth.globus.org\" "
-        f"--project-id \"{project_id}\" "
+        f"--project-name \"{project_name}\" "
         f"--organization \"{organization}\" "
         f"--contact-email \"{contact_email}\" "
         f"--agree-to-letsencrypt-tos "
@@ -159,23 +96,21 @@ def change_owner(config):
     run_command(cmd)
 
 def destroy(config):
-
-
-    # Step 3: Cleanup local node
+    # Step 1: Cleanup local node
     try:
         run_command("sudo globus-connect-server node cleanup")
         print(">>> Node cleanup complete.")
     except Exception as e:
         print(f"!!! Failed to cleanup node: {e}")
     
-    # Step 4: Cleanup endpoint
+    # Step 2: Cleanup endpoint
     try:
         run_command(f"sudo globus-connect-server endpoint cleanup --agree-to-delete-endpoint")
         print(">>> Endpoint cleanup complete.")
     except Exception as e:
         print(f"!!! Failed to cleanup endpoint: {e}")
 
-    # Step 5: Restart Apache services
+    # Step 3: Restart Apache services
     try:
         run_command("sudo systemctl restart apache2")
         run_command("sudo systemctl reload apache2")
@@ -183,7 +118,7 @@ def destroy(config):
     except Exception as e:
         print(f"!!! Failed to restart Apache: {e}")
 
-    # Step 6: Logout from Globus GCS session    
+    # Step 4: Logout from Globus GCS session    
     print(">>> Logging out of Globus GCS session")
     run_command("globus-connect-server logout --yes")
 
